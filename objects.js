f4659dbd3623b8962a7d5de4e834eed465a1b417
lcaptain47 <lcaptain47@tamu.edu>
2021-09-11 16:06:20 -0500

    Things 3

--- objects.js
+++ objects.js
@@ -1,15 +1,27 @@
-class clip {
+export class Clip {
     constructor(file, speed, volume){
         this.file = file;
         this.speed = 1.0;
         this.volume = .5;
     }
+    changeVolume(newVolume) {
+        this.volume = newVolume;
+    }
+    changeSpeed(newSpeed) {
+        this.speed = newSpeed;
+    }
 }
 
-var ayameTisI = new clip(file, 1.0, .5);
-var eekumBokum = new clip(file, 1.0, .5);
-var unforgivable = new clip(file, 1.0, .5);
-var hiHoney = new clip(file, 1.0, .5);
-var onigiri = new clip(file, 1.0, .5);
-var yubiYubi = new clip(file, 1.0, .5);
-var pineapple = new clip(file, 1.0, .5);
+export class SoundBites{
+    constructor(){
+        var ayameTisI = new Clip('\\assets\\Ayame_Tis_I.mp3', 1.0, .5);
+        var eekumBokum = new Clip('\\assets\\Eekum_Bokum.mp3', 1.0, .5);
+        var unforgivable = new Clip('\\assets\\unforgivable.mp3', 1.0, .5);
+        var hiHoney = new Clip('\\assets\\Hi_Honey.mp3', 1.0, .5);
+        var onigiri = new Clip('\\assets\\onigiri.mp3', 1.0, .5);
+        var yubiYubi = new Clip(file, 1.0, .5);
+        var pineapple = new Clip(file, 1.0, .5);
+    }
+    
+}
+
